0f825b4f98804b7d5c721787504431f399158478
Matthew Iannucci <mpiannucci@gmail.com>
2023-01-31 21:47:06 -0500

    Add dods test

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ pub mod data_type;
 pub mod dds;
 pub mod dods;
 
+pub use das::*;
+pub use dds::*;
+pub use dods::*;
+
 #[cfg(test)]
 mod tests {
     use byteorder::{BigEndian, ReadBytesExt};
--- tests/parse.rs
+++ tests/parse.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use readap::{das::parse_das_attributes, dds::DdsDataset};
+use readap::{data_type::DataArray, parse_das_attributes, DdsDataset, DodsDataset};
 
 #[test]
 fn read_das() {
@@ -16,9 +16,24 @@ fn read_das() {
 
 #[test]
 fn read_dds() {
-	let input = &fs::read_to_string("./data/44008.ncml.dds").unwrap();
+    let input = &fs::read_to_string("./data/44008.ncml.dds").unwrap();
 
-	let (_, dataset) = DdsDataset::parse(input).unwrap();
-	assert_eq!(dataset.values.len(), 16);
-	assert_eq!(dataset.name, "data/stdmet/44008/44008.ncml");
+    let (_, dataset) = DdsDataset::parse(input).unwrap();
+    assert_eq!(dataset.values.len(), 16);
+    assert_eq!(dataset.name, "data/stdmet/44008/44008.ncml");
+}
+
+#[test]
+fn read_dataset() {
+    let input = &fs::read("./data/swden/44097w9999.nc.dods").unwrap();
+
+    let dataset = DodsDataset::from_bytes(&input).unwrap();
+    let mwd = if let DataArray::Int32(mwd) = dataset.variable_data("mean_wave_dir").unwrap() {
+        mwd
+    } else {
+        vec![]
+    };
+
+    assert_eq!(mwd[0], 260);
+    assert_eq!(mwd[mwd.len() - 1], 188);
 }
