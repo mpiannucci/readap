2fdb680517ddf71356c923629241cc34f60633cb
Matthew Iannucci <mpiannucci@gmail.com>
2023-02-04 21:36:08 -0500

    Update errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,4 @@
-
+use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum Error {
@@ -6,4 +6,17 @@ pub enum Error {
 	ParseError,
 	InvalidTypecast,
 	NotImplemented,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = match &self {
+            Error::InvalidData => "InvalidData",
+            Error::ParseError => "ParseError",
+            Error::InvalidTypecast => "InvalidTypecast",
+            Error::NotImplemented => "NotImplemented",
+        };
+
+        f.write_str(message)
+    }
 }
